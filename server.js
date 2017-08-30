962f906b409426177496280a7939d537872ef356
hsootree <hsootree@gmail.com>
2017-08-30 16:02:30 +0900

    add randomstring

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express') // command + enter 뒤에 자동생성된  것을 건너뛰고 줄바꿈 단축키
 
-const data = [
-  {longUrl: 'http://google.com', id: '58DX37'} // 가짜 데이터 (목 데이터라고도 함.)
-]// 실제로 이렇게 배열을 정리하지는 않음. 
+// const data = [
+//   {longUrl: 'http://google.com', id: '58DX37'} // 가짜 데이터 (목 데이터라고도 함.)
+//] // 실제로 이렇게 배열을 정리하지는 않음. 
 // http://localhost:3000/58DX37 접속하면
 // 302 응답이 오고
 // 
@@ -11,7 +11,12 @@ const app = express()
 const morgan = require('morgan')
 // const app = require('express')()// 중복선언이 되어서 에러남. 
 const basicAuth = require('express-basic-auth')
+const randomstring = require('randomstring')
 
+
+const data = [
+  {longUrl: 'http://google.com', id: randomstring.generate(7)}
+]
 
 app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
@@ -33,6 +38,17 @@ app.get('/', (req, res) => {
   // res.render('index.ejs')
   res.render('index.ejs', {data})
   
+})
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+  const matched = data.find(item => item.id === id)
+  if (matched) {
+    res.redirect(301, matched.longUrl)
+  } else {
+    res.status(404)
+    res.send('404 Not Found')// 실무에서 이렇게 하면 사용자가 사용이 불편하여 떠난다.
+  }
 })
 
 app.listen(3000, () => {
